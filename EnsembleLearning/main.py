82a8cedc3d95421be2b2b282e0de0a083729393b
tjvilliard <u1120747@umail.utah.edu>
2022-10-21 17:42:27 -0600

    Updated assignment structure

--- EnsembleLearning/main.py
+++ EnsembleLearning/main.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 
 def main():
-
     assignment_2a()
 
-    #assignment_2b()
+    # assignment_2b()
 
-    #assignment_2c()
+    # assignment_2c()
 
-    #assignment_2d()
+    # assignment_2d()
+
 
 def assignment_2a():
     col_names = range(17)
@@ -31,27 +31,50 @@ def assignment_2a():
 
     test_errs = []
     train_errs = []
+
+    print("Logging AdaBoost Experiment Data...")
     exp = open("Experiments/ada_exp.csv", "w")
-    st_data = open("Experiments/stump_data.csv", "w")
 
     exp.write("t, train, test \n")
-    st_data.write("")
-    for t in range(1, 502, 50):
+    stump_tr_errors = []
+    stump_test_errors = []
+    for t in range(1, 11):
         print(t)
         ensemble = AdaBoost(train, t)
 
         train_pred = ensemble.predict(x_train)
+        for stump_pred in ensemble.votes:
+            stump_tr_errors.append(prediction_error(y_train, stump_pred))
+
         test_pred = ensemble.predict(x_test)
+        for stump_pred in ensemble.votes:
+            stump_test_errors.append(prediction_error(y_test, stump_pred))
 
         exp.write(str(t) + ", " + str(prediction_error(y_train, train_pred)) + ", " + str(
             prediction_error(y_test, test_pred)) + "\n")
+    exp.close()
+    print("Complete: Experiments/ada_exp.csv \n")
+
+    print("Logging Stump Errors... ")
+    st_data = open("Experiments/stump_data.csv", "w")
+    st_data.write("train, test \n")
+    for i in range(len(stump_tr_errors)):
+        st_data.write(str(stump_tr_errors[i]) + ", " + str(stump_test_errors[i]) + "\n")
+    st_data.close()
+    print("Complete: Experiments/stump_data.csv \n")
 
 
 def assignment_2b():
+    return
+
 
 def assignment_2c():
+    return
+
 
 def assignment_2d():
+    return
+
 
 if __name__ == "__main__":
     main()

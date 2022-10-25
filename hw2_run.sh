3020b8a57da31e0a460a675d94d9c461448d4369
Tj <tjvilliard@gmail.com>
2022-10-24 21:15:57 -0600

    Adjusted hw2_run.sh

--- hw2_run.sh
+++ hw2_run.sh
@@ -4,3 +4,4 @@ pip3 install matplotlib
 pip3 install pandas
 python3 EnsembleLearning/main.py
 python3 LinearRegression/main.py
+echo "all experiment data logged in EnsembleLearning/Experiments and LinearRegression/Experiments"
